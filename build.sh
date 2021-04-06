d360098b1962b2904f843bae9fa4b09744da6af9
Kyle Robert Steffen <krsteffen@utexas.edu>
2021-04-06 12:10:14 -0500

    new cmake using "add_subdirectory" to recursively build NEMS app

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -eu
+#set -eu
+
+# Note: Needed to install the following.
+# NetCDF_INCLUDE_DIRS=${TACC_NETCDF_INC} make nemsio sp w3emc sigio
 
 if [[ $(uname -s) == Darwin ]]; then
   readonly UFS_MODEL_DIR=$(cd "$(dirname "$(greadlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
@@ -11,8 +14,14 @@ export CMAKE_C_COMPILER=${CMAKE_C_COMPILER:-${CC:-mpicc}}
 export CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER:-${CXX:-mpicxx}}
 export CMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER:-${FC:-mpif90}}
 
+module load esmf nems python3
+
 export NETCDF=${NETCDF:-${NETCDF_ROOT:?"Please set NETCDF/NETCDF_ROOT environment variable"}}
 export ESMFMKFILE=${ESMFMKFILE:?"Please set ESMFMKFILE environment variable"}
+
+export MYNCEPLIBS=$HOME/opt/nceplibs
+export WW3_COMP=stampede.intel
+export CMAKE_PREFIX_PATH=$MYNCEPLIBS/bacio-2.4.1:$MYNCEPLIBS/nemsio-2.5.2:$MYNCEPLIBS/w3nco-2.4.1:$MYNCEPLIBS/sp-2.3.3:$MYNCEPLIBS/w3emc-2.7.3:$MYNCEPLIBS/sigio-2.3.2
 
 BUILD_DIR=${BUILD_DIR:-${UFS_MODEL_DIR}/build}
 mkdir -p ${BUILD_DIR}
